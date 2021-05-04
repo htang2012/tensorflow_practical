8aed467a26e833688425d0ea3e17bbf637c82cf5
Henry Tang <henry.yu.tang@gmail.com>
2021-05-04 11:39:44 -0700

    add distributed training.

---
+++ 11_distributed/multigpu_simulated.py
@@ -0,0 +1,86 @@
+
+#https://github.com/htang2012/eat_tensorflow2_in_30_days/blob/master/6-4%2C%E4%BD%BF%E7%94%A8%E5%A4%9AGPU%E8%AE%AD%E7%BB%83%E6%A8%A1%E5%9E%8B.md
+import tensorflow as tf
+
+print(tf.__version__)
+from tensorflow.keras import * 
+
+#打印时间分割线
+@tf.function
+def printbar():
+    ts = tf.timestamp()
+    today_ts = ts%(24*60*60)
+
+    hour = tf.cast(today_ts//3600+8,tf.int32)%tf.constant(24)
+    minite = tf.cast((today_ts%3600)//60,tf.int32)
+    second = tf.cast(tf.floor(today_ts%60),tf.int32)
+    
+    def timeformat(m):
+        if tf.strings.length(tf.strings.format("{}",m))==1:
+            return(tf.strings.format("0{}",m))
+        else:
+            return(tf.strings.format("{}",m))
+    
+    timestring = tf.strings.join([timeformat(hour),timeformat(minite),
+                timeformat(second)],separator = ":")
+    tf.print("=========="*8,end = "")
+    tf.print(timestring)
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # 设置两个逻辑GPU模拟多GPU训练
+  try:
+    tf.config.experimental.set_virtual_device_configuration(gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024),
+         tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPU,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    print(e)
+
+
+MAX_LEN = 300
+BATCH_SIZE = 32
+(x_train,y_train),(x_test,y_test) = datasets.reuters.load_data()
+x_train = preprocessing.sequence.pad_sequences(x_train,maxlen=MAX_LEN)
+x_test = preprocessing.sequence.pad_sequences(x_test,maxlen=MAX_LEN)
+
+MAX_WORDS = x_train.max()+1
+CAT_NUM = y_train.max()+1
+
+ds_train = tf.data.Dataset.from_tensor_slices((x_train,y_train)) \
+          .shuffle(buffer_size = 1000).batch(BATCH_SIZE) \
+          .prefetch(tf.data.experimental.AUTOTUNE).cache()
+   
+ds_test = tf.data.Dataset.from_tensor_slices((x_test,y_test)) \
+          .shuffle(buffer_size = 1000).batch(BATCH_SIZE) \
+          .prefetch(tf.data.experimental.AUTOTUNE).cache()
+
+tf.keras.backend.clear_session()
+def create_model():
+
+    model = models.Sequential()
+
+    model.add(layers.Embedding(MAX_WORDS,7,input_length=MAX_LEN))
+    model.add(layers.Conv1D(filters = 64,kernel_size = 5,activation = "relu"))
+    model.add(layers.MaxPool1D(2))
+    model.add(layers.Conv1D(filters = 32,kernel_size = 3,activation = "relu"))
+    model.add(layers.MaxPool1D(2))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(CAT_NUM,activation = "softmax"))
+    return(model)
+
+def compile_model(model):
+    model.compile(optimizer=optimizers.Nadam(),
+                loss=losses.SparseCategoricalCrossentropy(),
+                metrics=[metrics.SparseCategoricalAccuracy(),metrics.SparseTopKCategoricalAccuracy(5)])
+    return(model)
+
+strategy = tf.distribute.MirroredStrategy()
+
+with strategy.scope():
+    model = create_model()
+    model.summary()
+    model = compile_model(model)
+
+history = model.fit(ds_train,validation_data = ds_test,epochs = 10)
